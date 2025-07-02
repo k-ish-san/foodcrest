f17d3a404c407f1009deb5ca27ff06cd6ebeda98
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-07-02 18:39:01 +0530

    Filter fix

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,13 +1,19 @@
 import DishCard from "./DishCard.jsx";
 import dishes from "../components/data/dishes";
 
-const Dishes = () => {
+const Dishes = ({ selectedCategory }) => {
+  const filteredDishes = selectedCategory
+    ? dishes.filter((dish) => dish.category === selectedCategory)
+    : dishes;
+
   return (
     <section className="py-12 px-6 max-w-[1400px] mx-auto">
-      <h2 className="text-3xl font-bold text-center mb-8">Our Dishes</h2>
+      <h2 className="text-3xl font-bold text-center mb-8">
+        {selectedCategory ? `${selectedCategory} Dishes` : "Our Dishes"}
+      </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-        {dishes.map((dish, index) => (
+        {filteredDishes.map((dish, index) => (
           <DishCard key={index} dish={dish} />
         ))}
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import HeroSection from "../components/HeroSection";
 import About from "../components/About";
 import Dishes from "../components/Dishes";
 import Reviews from "../components/Reviews";
-import { useState } from "react";
 
-const Home = () => {
-  const [selectedCategory] = useState(""); // "" means show all by default
+
+const Home = ({selectedCategory}) => {
+  
 
   return (
     <>
